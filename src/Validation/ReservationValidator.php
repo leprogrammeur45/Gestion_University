b5966bee1933200e740e15b45c4197bc268edc3a
leprogrammeur <p0846714@gmail.com>
2026-09-07 17:33:39 +0000

    fix: adapt reservation validation to form input

--- src/Validation/ReservationValidator.php
+++ src/Validation/ReservationValidator.php
@@ -11,33 +11,57 @@ class ReservationValidator implements ValidatorInterface
         $errors = [];
 
         // salle_id
-        if (!isset($data['salle_id']) || !v::intType()->positive()->validate($data['salle_id'])) {
-            $errors['salle_id'][] = 'L\'identifiant de la salle doit être un entier positif.';
+        if (
+            !isset($data['salle_id']) ||
+            !v::stringType()->digit()->notEmpty()->validate($data['salle_id'])
+        ) {
+            $errors['salle_id'][] =
+                'L\'identifiant de la salle doit être un entier positif.';
         }
 
         // responsable
-        if (!isset($data['responsable']) || !v::stringType()->length(2, 120)->validate($data['responsable'])) {
-            $errors['responsable'][] = 'Le responsable doit contenir entre 2 et 120 caractères.';
+        if (
+            !isset($data['responsable']) ||
+            !v::stringType()->length(2, 120)->validate($data['responsable'])
+        ) {
+            $errors['responsable'][] =
+                'Le responsable doit contenir entre 2 et 120 caractères.';
         }
 
         // email
-        if (!isset($data['email']) || !v::email()->validate($data['email'])) {
-            $errors['email'][] = 'L\'adresse email est invalide.';
+        if (
+            !isset($data['email']) ||
+            !v::email()->validate($data['email'])
+        ) {
+            $errors['email'][] =
+                'L\'adresse email est invalide.';
         }
 
         // motif
-        if (!isset($data['motif']) || !v::stringType()->length(5, 255)->validate($data['motif'])) {
-            $errors['motif'][] = 'Le motif doit contenir entre 5 et 255 caractères.';
+        if (
+            !isset($data['motif']) ||
+            !v::stringType()->length(5, 255)->validate($data['motif'])
+        ) {
+            $errors['motif'][] =
+                'Le motif doit contenir entre 5 et 255 caractères.';
         }
 
         // date_debut
-        if (!isset($data['date_debut']) || !v::dateTime('Y-m-d H:i:s')->validate($data['date_debut'])) {
-            $errors['date_debut'][] = 'La date de début est invalide.';
+        if (
+            !isset($data['date_debut']) ||
+            !v::dateTime('Y-m-d\TH:i')->validate($data['date_debut'])
+        ) {
+            $errors['date_debut'][] =
+                'La date de début est invalide.';
         }
 
         // date_fin
-        if (!isset($data['date_fin']) || !v::dateTime('Y-m-d H:i:s')->validate($data['date_fin'])) {
-            $errors['date_fin'][] = 'La date de fin est invalide.';
+        if (
+            !isset($data['date_fin']) ||
+            !v::dateTime('Y-m-d\TH:i')->validate($data['date_fin'])
+        ) {
+            $errors['date_fin'][] =
+                'La date de fin est invalide.';
         }
 
         return new ValidationResult(
